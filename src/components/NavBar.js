71353152b982bacb19bd3ab5c0e03ef7fd4130a6
Edwin Paul <saidarajab101@gmail.com>
2023-04-24 13:25:51 +0300

    list & Keys Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <a key="home-link" href="#home">home</a>
+      <a key="about-link" href="#about">about</a>
+      <a key="projects-link" href="#projects">projects</a>
+    </nav>
+  );
 }
 
 export default NavBar;
